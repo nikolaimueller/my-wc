d127113cb59151179eb942c10eef2169161684bc
Nikolai Müller <nikolai.mueller@nikim.de>
2020-10-17 11:24:43 +0200

    Routing: Switch view on loading url hash-bang - to do: still an ugly hack!!

--- components/routing/route-view.js
+++ components/routing/route-view.js
@@ -36,7 +36,19 @@ export default class RouteView extends HTMLElement {
     connectedCallback() {
         // Invoked each time the custom element is appended into a document-connected element.
         if (this.isConnected) {
-            // Initial switch-route
+            // *** Initial switch-route ***
+            let locationHash = location.hash
+            if (locationHash === '#/') {
+                // Ignore empty route url.
+                locationHash = ''
+            } else if (locationHash.length > 2 && locationHash.startsWith('#/')) {
+                locationHash = locationHash.substr(1) // Drop leading '#' (hashbang).
+                let matchRoute = lookupRouteByUrl(locationHash)
+                if (!matchRoute) {
+                    // Redirect to location.hash only if a matching route has been found.
+                    locationHash = ''
+                }
+            }
             let currentRoute = getCurrentRoute()
             let defaultRoute = getDefaultRoute()
             let newUrl = null;
@@ -46,11 +58,39 @@ export default class RouteView extends HTMLElement {
             if (currentRoute !== null) {
                 newUrl = currentRoute.url
             }
+            if (locationHash.length > 1) {
+                newUrl = locationHash
+            }
+            // console.log(`+++ ${RouteView.tag}.connectedCallback: - locationHash: '${locationHash}' - currentRoute?.url: '${currentRoute?.url}' - defaultRoute?.url: '${defaultRoute?.url}' => newUrl: '${newUrl}'`)
             switchRoute(newUrl)
         }
     }
 
     switchContent(currentRoute, newUrl) {
+        function doSwitchContent(currentRoute, newRoute) {
+            // Switch the displayed component
+            if (!currentRoute || newRoute.url !== currentRoute.url) {
+                // ..Remove old routeView content node (child).
+                if (this.refRoutingView) {
+                    this.shadowRoot.removeChild(this.refRoutingView)
+                }
+                // ...append new created content node to routeView.
+                this.refRoutingView = document.createElement(newRoute.component.tag)
+                
+                // $$$ TODO: Handle Themes.
+                this.refRoutingView.setAttribute('theme', 'theme')
+
+                this.shadowRoot.appendChild(this.refRoutingView)
+
+                // Reflect current route into history state
+                let stateObj = null
+                history.replaceState(stateObj, `route: ${newRoute.url}`, `#${newRoute.url}`)
+
+                return newRoute
+            }
+            return null
+        }
+
         // console.log(`${RouteView.tag}.switchContent-0: defaultRoute?.url: '${defaultRoute?.url}' -- newUrl: '${newUrl}' -- currentRoute?.tag: <${defaultRoute?.tag}>`)
 
         let newRoute = lookupRouteByUrl(newUrl)
@@ -62,6 +102,28 @@ export default class RouteView extends HTMLElement {
             return null
         }
 
+        // Handle async loading route's component
+        // $$$
+        // $$$ WORKAROUND: THIS IS A BAD HACK !!
+        // $$$ - BETTER-SOLUTION: turn route's 'component' property into Promise, delivering component async !! $$$
+        // $$$
+        let fuSwitchContent = doSwitchContent.bind(this)
+        if (newRoute.component && (typeof newRoute.component === 'string' || newRoute.component instanceof String)) {
+            console.warn('*** <route-view>.switchContent(): newRoute.component - stil loading async(!) - wait some time ...')
+            let msecs = 500
+            setTimeout(function () {
+                if (newRoute.component && (typeof newRoute.component === 'string' || newRoute.component instanceof String)) {
+                    console.error(`*** <route-view>.switchContent(): newRoute.component - stil loading async! (after ${msecs} msec) - TIMEOUT(!)`)
+                } else {
+                    console.warn(`*** <route-view>.switchContent(): newRoute.component - ASYNC LOADED (after ${msecs} msecs)`)
+                    return fuSwitchContent(currentRoute, newRoute)
+                }
+            }, msecs)
+        } else {
+            return fuSwitchContent(currentRoute, newRoute)
+        }
+
+        /*
         // Switch the displayed component
         if (!currentRoute || newRoute.url !== currentRoute.url) {
             // ..Remove old routeView content node (child).
@@ -83,6 +145,7 @@ export default class RouteView extends HTMLElement {
             return newRoute
         }
         return null
+        */
     }
 }
 // Register custom element.
